9d72b0fd510df790536a0020925fec1d1a280809
Matthew Boubin <mjb9353@nyu.edu>
2025-03-11 16:35:41 -0400

    added capstone data to df

--- main.py
+++ main.py
@@ -3,5 +3,6 @@ from capstoneUpdate import CapstoneUpdate
 capstone24 = CapstoneUpdate()
 df = capstone24.rest_import()
 capstone24.sf()
+print(df)
 #capstone24.format_focus_areas()
 #capstone24.preview_df()
